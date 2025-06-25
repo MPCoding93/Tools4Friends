b45b8603bfe971f75163ed932288f7fe42e064b7
MPCoding93 <162565577+MPCoding93@users.noreply.github.com>
2025-06-25 12:34:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta
-      name="description"
-      content="Borrowing tools from friends for friends"
-    />
-    <meta name="keywords" content="Tools for Friends, tools, naradi" />
-    <meta name="author" content="MPCoding" />
-    <link rel="stylesheet" href="styles.css" />
-    <link rel="icon" href="/favicon-dark.ico" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-      rel="stylesheet"
-    />
     <script src="script.js" defer></script>
-
     <title>Tools4Friends</title>
   </head>
-
   <body>
     <div class="container">
       <header>
@@ -33,20 +12,7 @@
         </div>
       </header>
       <div class="line-break"></div>
-
-      <nav>
-        <div class="nav-left">
-          <a href="index.html" data-en="Home" data-cs="Domů">Home</a>  
-          <a href="tools.php" data-en="Tools" data-cs="Nářadí">Tools</a>  
-          <a href="contacts.html" data-en="Contacts" data-cs="Kontakty">Contacts</a>
-          <a href="login.php" data-en="Login" data-cs="Přihlásit">Login</a>
-        </div>
-
-        <div class="nav-right language-toggle">
-          <button onclick="switchLanguage('en', 'index.html')">English</button>    
-          <button onclick="switchLanguage('cs', 'index.html')">Čeština</button>  
-        </div>
-      </nav>
+      <?php include 'navbar.php'; // Include the navbar ?>
 
       <main>
         <h1
