907053076af545d29c03c1253816d9a7d882dc2e
MPCoding93 <162565577+MPCoding93@users.noreply.github.com>
2025-07-03 10:41:00 +0200

    Update navbar.php

--- app/navbar.php
+++ app/navbar.php
@@ -17,10 +17,10 @@ $currentPageBasename = basename($_SERVER['PHP_SELF']);
         <a href="/index.php?lang=<?php echo $lang; ?>" data-en="Home" data-cs="Domů">
             <?php echo $lang === 'cs' ? 'Domů' : 'Home'; ?>
         </a>
-        <a href="public/tools.php?lang=<?php echo $lang; ?>" data-en="Tools" data-cs="Nářadí">
+        <a href="./tools.php?lang=<?php echo $lang; ?>" data-en="Tools" data-cs="Nářadí">
             <?php echo $lang === 'cs' ? 'Nářadí' : 'Tools'; ?>
         </a>
-        <a href="public/contacts.php?lang=<?php echo $lang; ?>" data-en="Contacts" data-cs="Kontakty">
+        <a href="./contacts.php?lang=<?php echo $lang; ?>" data-en="Contacts" data-cs="Kontakty">
             <?php echo $lang === 'cs' ? 'Kontakty' : 'Contacts'; ?>
         </a>
 
