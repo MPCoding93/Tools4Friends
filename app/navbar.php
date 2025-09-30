74eda21d80112c28dd55b42cc9939c82f5ff141a
MPCoding93 <162565577+MPCoding93@users.noreply.github.com>
2025-09-30 21:10:38 +0200

    Update navbar.php

--- app/navbar.php
+++ app/navbar.php
@@ -52,6 +52,6 @@ $currentPageBasename = basename($_SERVER['PHP_SELF']);
     </div>
 </nav>
 <br>
-    <div>
+    <div class="UnderConstruction">
         <h1>SITE UNDER CONSTRUCTION</h1>
     </div>
