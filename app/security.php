adf5e507205dd80ba26f8141b7a62656916f8d9a
Pauwels <162565577+MPCoding93@users.noreply.github.com>
2025-10-27 13:08:46 +0100

    Update TimeOut Page

--- app/security.php
+++ app/security.php
@@ -50,12 +50,12 @@ function setSecurityHeaders() {
 
 function checkSessionTimeout() {
     $timeout = defined('SESSION_LIFETIME') ? SESSION_LIFETIME : 1800;
-    
-    if (isset($_SESSION['last_activity']) && 
+
+    if (isset($_SESSION['last_activity']) &&
         (time() - $_SESSION['last_activity'] > $timeout)) {
         session_unset();
         session_destroy();
-        header("Location: /Tools4Friends/public/login.php?timeout=1");
+        header("Location: /Tools4Friends/index.php");
         exit();
     }
     $_SESSION['last_activity'] = time();
